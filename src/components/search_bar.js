8d7d89e3dd3fd829424e6e43dbe31da425ad8c1e
Jiho <jihotahk@gmail.com>
2017-03-30 16:21:48 -0500

    started on state-driven searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,6 +1,56 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-const SearchBar = () => {
-    return <input />;
-};
+
+// create a class-based component, based off of React.Component
+class SearchBar extends Component {
+
+    //all js classese have a special fx called constructor
+    // first and only fx called automatically Whenever new instance of class is created
+    // used to initialize variables like state
+    constructor(props) {
+        // super calls parent class (React.Component)
+        super(props);
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div>
+                <input
+                    value = {this.state.term}
+                    onChange = { event => this.setState({ term:event.target.value })} />
+            </div>
+        );
+
+    }
+}
+    // every class must have a render function
+    // render() {
+    //     // render fx must return some JSX, otherwise error
+    //     return <input onChange={this.onInputChange} />;
+    // }
+    //
+    // // event handler
+    // onInputChange(event) {
+    //     setState({ term: event.target.value });
+    // }
+
+    /*
+    Short hand for one liner event handler on render function:
+    return <input onChange = { (event) => console.log(event.target.value) } />;
+
+    if only one argument, can even remove ()
+
+    */
+
+
+export default SearchBar;
+
+/*/ 'State' is a plain javascript object that is used to record or react to user events.
+// Each class-based component has its own state object.
+// Whenever a component's state is changed, the component automatically re renders.
+// Also forces its children to re render as well.
+Before using STATE inside of a component, we need to initialize the state object.
+To initialize STATE, we set property 'state' to a plain js object inside the class' constructor object.
+*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// for our own js imports, write out location without file type
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAILMrrqaa6K6sITyhpnBU_57cw0F1th5s'
 
 //create a new component that will produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 // take generated HTML and append on DOM
